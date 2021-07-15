b86a5a87fcc78a632f99a0754f56dcb9706f02f5
k.nakamura <k.nakamura@magic-plus.net>
2021-07-15 13:59:33 +0900

    コメント変更

--- Homestead/code/laravelTest/app/Ver.php
+++ Homestead/code/laravelTest/app/Ver.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ver extends Model
 {
-    //20210618
+    //202100715
     protected $table = 'ver';
     protected $dates =  ['created_at', 'updated_at'];
     protected $fillable = ['id', 'version', 'min_version'];
